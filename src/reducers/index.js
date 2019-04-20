9787c0853f438ebd62e0f42141702ab8d3720518
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-04-20 14:48:30 +0630

    Remove unnecessary reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 import {combineReducers} from 'redux';
-import messages from './messages';
-import typing_status from './typing';
 import currentUser from './currentUser';
 
-export default combineReducers({messages,typing_status,currentUser});
+export default combineReducers({currentUser});
